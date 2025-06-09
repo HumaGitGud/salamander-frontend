ebf6290f4b35799e3431c7c59d95ec2c51bc9405
HumaGitGud <khomidov.huma@student.greenriver.edu>
2025-06-09 16:01:44 -0700

    implemented job status using interval

--- src/app/preview/[filename]/page.js
+++ src/app/preview/[filename]/page.js
@@ -1,5 +1,6 @@
 // Preview page (display video information when clicked from videos list)
 // Still need to implement color picker and threshold to pass correct URL to fetch req
+// Still need to do proper tests
 
 'use client';
 
@@ -17,6 +18,7 @@ export default function PreviewPage({ params }) {
     return `http://localhost:3000${src}?w=${width}`;
   };
 
+  // make a POST request with a given link (targetColor and threshold are hardcoded rn)
   const handleStartProcess = async () => {
     try {
       const res = await fetch(`http://localhost:3000/process/${filename}?targetColor=255,0,0&threshold=50`, {
@@ -33,6 +35,31 @@ export default function PreviewPage({ params }) {
     }
   };
 
+  // fetch and display job status using interval
+  useEffect(() => {
+    if (!jobId) return;
+
+    const interval = setInterval(async () => {
+      try {
+        const res = await fetch(`http://localhost:3000/process/${jobId}/status`);
+        if (!res.ok) throw new Error("Failed to fetch status");
+
+        const data = await res.json();
+        setStatus(data.status);
+        if (data.status === 'done') {
+          setResultFile(data.result);
+          clearInterval(interval);
+        }
+      } catch (err) {
+        console.error("Error fetching job status:", err);
+        clearInterval(interval);
+      }
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, [jobId]);
+
+  // UI elements
   return (
     <div>
       <h1>Preview Page</h1>
@@ -48,6 +75,13 @@ export default function PreviewPage({ params }) {
       <button onClick={handleStartProcess} style={{ display:'block'}}>
         Start Process
       </button>
+
+      {status && <p>Status: <strong>{status}</strong></p>}
+
+      {status === 'done' && resultFile && (
+        <a href={`http://localhost:3000/results/${resultFile}`}> Download CSV result </a>
+      )}
+
     </div>
   );
 
